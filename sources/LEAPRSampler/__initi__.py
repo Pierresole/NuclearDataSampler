1611ef94b93e2e860639054c2bd1e9e78617f142
Pierresole <pierre.sole@gmail.com>
2025-01-20 15:59:18 +0100

    create subdirs

---
+++ sources/LEAPRSampler/__initi__.py
@@ -0,0 +1,5 @@
+from .leaprParser import PerturbLeaprInput
+from .leaprParser import perturb_all
+
+# Exported names when doing "from NDSampler import *"
+# __all__ = ["main_function", "helper_function"]
---
+++ sources/NDSampler/__initi__.py
@@ -0,0 +1,4 @@
+from .ENDFSampler.py import main_function
+
+# Exported names when doing "from NDSampler import *"
+__all__ = ["main_function", "helper_function"]
